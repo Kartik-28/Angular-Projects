6f03fc5f5339455d10485ba5f433c91b60ea6e7e
Kartik-28 <kartikkuk@gmail.com>
2021-12-29 14:15:11 -0500

    Created recipes component

--- Shopping-cart-and-recepie-book-app/src/app/app.module.ts
+++ Shopping-cart-and-recepie-book-app/src/app/app.module.ts
@@ -4,15 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipiesComponent } from './recipies/recipies.component';
-import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent,
-    RecipieListComponent
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
