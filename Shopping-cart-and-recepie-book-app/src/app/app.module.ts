1a25765f9595f96bb308fc7f65c4c700580af5eb
Kartik-28 <kartikkuk@gmail.com>
2021-12-29 14:06:58 -0500

    Created recepie-list component

--- Shopping-cart-and-recepie-book-app/src/app/app.module.ts
+++ Shopping-cart-and-recepie-book-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipieListComponent } from './recipies/recipie-list/recipie-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipiesComponent
+    RecipiesComponent,
+    RecipieListComponent
   ],
   imports: [
     BrowserModule,
